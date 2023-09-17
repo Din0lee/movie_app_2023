ffdcd61383ccfd37968bdd14003c96b1cf421ef4
Din0lee <act0920@naver.com>
2023-09-17 16:01:26 +0900

    Proptypes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
-
-function Food({name,picture}){
-  return <div>
-    <h2>I Like {name}</h2>
-    <img src= {picture} />
-  </div>
-}
+import React from "react";
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
+    id : 1,
     name : "Kimchi",
-    image : "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+    image : "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating : 4.5
   },
   {
+    id : 2,
     name : "Samgyeopsal",
-    image : "http://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"
+    image : "http://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
+    rating : 5
   },
   {
+    id : 3,
     name : "Doncasu",
-    image : "https://cdn.funshop.co.kr/products/0000076768/vs_image800.jpg"
+    image : "https://cdn.funshop.co.kr/products/0000076768/vs_image800.jpg",
+    rating : 4.8
   },
   {
+    id : 4,
     name: "samgyetang",
-    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating : 4.9
   }
 ]
 
-function renderFood(dish){
-  return <Food name={dish.name} picture={dish.image}/>;
+function Food({name,picture,rating}){
+  return <div>
+    <h2>I Like {name}</h2>
+    <img src= {picture} alt={name} />
+    <h4>{rating}/5.0</h4>
+  </div>
 }
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
     <div className="App">
-      {console.log(foodILike.map(renderFood))}
-      {foodILike.map(renderFood)}
+      {foodILike.map(dish => (
+        <Food key={dish.id} 
+        name={dish.name} 
+        picture={dish.image} 
+        rating={dish.rating}/>
+      ))}
     </div>
   );
 }
